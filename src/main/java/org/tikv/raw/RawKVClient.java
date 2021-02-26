1195d9eb64baf9dc7c4116fbc5bbc4151b498d28
birdstorm <samuelwyf@hotmail.com>
2021-02-26 13:33:24 +0800

    add log
    Signed-off-by: birdstorm <samuelwyf@hotmail.com>

--- src/main/java/org/tikv/raw/RawKVClient.java
+++ src/main/java/org/tikv/raw/RawKVClient.java
@@ -383,16 +383,18 @@ public class RawKVClient implements AutoCloseable {
 
     if (oldRegion.equals(currentRegion)) {
       try (RegionStoreClient client = clientBuilder.build(batch.region); ) {
+        logger.info(Thread.currentThread().getName() + " - region unchanged");
         client.rawBatchPut(backOffer, batch, ttl);
         return new ArrayList<>();
       } catch (final TiKVException e) {
         // TODO: any elegant way to re-split the ranges if fails?
         backOffer.doBackOff(BackOffFunction.BackOffFuncType.BoRegionMiss, e);
-        logger.warn("ReSplitting ranges for BatchPutRequest");
+        logger.warn(Thread.currentThread().getName() + " - ReSplitting ranges for BatchPutRequest");
         // retry
         return doSendBatchPutWithRefetchRegion(backOffer, batch, ttl);
       }
     } else {
+      logger.info(Thread.currentThread().getName() + " - region changed");
       return doSendBatchPutWithRefetchRegion(backOffer, batch, ttl);
     }
   }
