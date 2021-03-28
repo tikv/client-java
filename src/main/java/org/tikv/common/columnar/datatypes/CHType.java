c0f5bde577951fbf58745abd56526971d66901f2
Peter Levart <peter.levart@gmail.com>
2021-03-28 21:49:47 +0200

    Allow building on JDK9+ by getting rid of DirectByteBuffer dependency
    Signed-off-by: Peter Levart <peter.levart@gmail.com>

--- src/main/java/org/tikv/common/columnar/datatypes/CHType.java
+++ src/main/java/org/tikv/common/columnar/datatypes/CHType.java
@@ -15,7 +15,7 @@
 
 package org.tikv.common.columnar.datatypes;
 
-import static org.tikv.common.util.MemoryUtil.allocateDirect;
+import static org.tikv.common.util.MemoryUtil.allocate;
 
 import java.nio.ByteBuffer;
 import org.tikv.common.codec.CodecDataInput;
@@ -41,7 +41,7 @@ public abstract class CHType {
 
   protected ByteBuffer decodeNullMap(CodecDataInput cdi, int size) {
     // read size * uint8 from cdi
-    ByteBuffer buffer = allocateDirect(size);
+    ByteBuffer buffer = allocate(size);
     MemoryUtil.readFully(buffer, cdi, size);
     buffer.clear();
     return buffer;
@@ -63,13 +63,13 @@ public abstract class CHType {
     }
     if (isNullable()) {
       ByteBuffer nullMap = decodeNullMap(cdi, size);
-      ByteBuffer buffer = allocateDirect(bufferSize(size));
+      ByteBuffer data = allocate(bufferSize(size));
       // read bytes from cdi to buffer(off-heap)
-      MemoryUtil.readFully(buffer, cdi, bufferSize(size));
-      buffer.clear();
-      return new TiBlockColumnVector(this, nullMap, buffer, size, length);
+      MemoryUtil.readFully(data, cdi, bufferSize(size));
+      data.clear();
+      return new TiBlockColumnVector(this, nullMap, data, size, length);
     } else {
-      ByteBuffer buffer = allocateDirect(bufferSize(size));
+      ByteBuffer buffer = allocate(bufferSize(size));
       MemoryUtil.readFully(buffer, cdi, bufferSize(size));
       buffer.clear();
       return new TiBlockColumnVector(this, buffer, size, length);
