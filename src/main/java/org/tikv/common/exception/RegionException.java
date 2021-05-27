34d5d76e310113edf7ad1b454e18cfd6447da8fd
birdstorm <samuelwyf@hotmail.com>
2021-05-27 17:06:40 +0900

    cherry pick #172 to release-3.1
    Signed-off-by: ti-srebot <ti-srebot@pingcap.com>

--- src/main/java/org/tikv/common/exception/RegionException.java
+++ src/main/java/org/tikv/common/exception/RegionException.java
@@ -24,7 +24,7 @@ public class RegionException extends TiKVException {
   private final Error regionErr;
 
   public RegionException(Error regionErr) {
-    super("Region Exception occurred" + regionErr.getMessage());
+    super("Region Exception occurred " + regionErr.getMessage());
     this.regionErr = regionErr;
   }
 
--- src/main/java/org/tikv/common/util/Batch.java
+++ src/main/java/org/tikv/common/util/Batch.java
@@ -56,7 +56,7 @@ public class Batch {
   }
 
   public BackOffer getBackOffer() {
-    return backOffer;
+    return ConcreteBackOffer.create(backOffer);
   }
 
   public TiRegion getRegion() {
