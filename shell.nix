3be4daf78c75891f91c36202063dc941ddfccc1b
Iosmanthus Teng <MyOsmanthusTree@gmail.com>
2021-06-09 09:48:21 +0800

    fix build issue in `nix-shell --pure` (#190)
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- shell.nix
+++ shell.nix
@@ -3,7 +3,12 @@
 (
   pkgs.buildFHSUserEnv {
     name = "client-java-shell";
-    targetPkgs = pkgs: with pkgs;[ maven openjdk8 git ];
-    runScript = "bash";
+    targetPkgs = pkgs: with pkgs;[ git maven openjdk8 ];
+    runScript = ''
+      env \
+       GIT_SSL_CAINFO=/etc/ssl/certs/ca-certificates.crt \
+       JAVA_HOME=${pkgs.openjdk8}/lib/openjdk \
+       bash
+    '';
   }
 ).env
