9c14257ff49a62608687ba04282c19cb21c6a34f
Andy Lok <andylokandy@hotmail.com>
2021-06-11 18:30:53 +0800

    Format code
    Signed-off-by: Andy Lok <andylokandy@hotmail.com>

--- src/main/java/org/tikv/raw/RawKVClient.java
+++ src/main/java/org/tikv/raw/RawKVClient.java
@@ -141,7 +141,8 @@ public class RawKVClient implements AutoCloseable {
    * @param key key
    * @param value value
    */
-  public void compareAndSet(ByteString key, Optional<ByteString> prevValue, ByteString value)  throws RawCASConflictException {
+  public void compareAndSet(ByteString key, Optional<ByteString> prevValue, ByteString value)
+      throws RawCASConflictException {
     compareAndSet(key, prevValue, value, 0L);
   }
 
