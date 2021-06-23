302df232ef09d3e9d6e9f2c6e1ee9aefa803ce72
birdstorm <samuelwyf@hotmail.com>
2021-06-23 16:13:00 +0800

    fix test
    Signed-off-by: birdstorm <samuelwyf@hotmail.com>

--- src/test/java/org/tikv/common/PDClientTest.java
+++ src/test/java/org/tikv/common/PDClientTest.java
@@ -46,14 +46,16 @@ public class PDClientTest extends PDMockServerTest {
     try (PDClient client = session.getPDClient()) {
       client.trySwitchLeader("http://" + LOCAL_ADDR + ":" + (pdServer.port + 1));
       assertEquals(
-          client.getPdClientWrapper().getLeaderInfo(), LOCAL_ADDR + ":" + (pdServer.port + 1));
+          "http://" + LOCAL_ADDR + ":" + (pdServer.port + 1),
+          client.getPdClientWrapper().getLeaderInfo());
     }
     tearDown();
     setUp(LOCAL_ADDR_IPV6);
     try (PDClient client = session.getPDClient()) {
       client.trySwitchLeader("http://" + LOCAL_ADDR_IPV6 + ":" + (pdServer.port + 2));
       assertEquals(
-          client.getPdClientWrapper().getLeaderInfo(), LOCAL_ADDR_IPV6 + ":" + (pdServer.port + 2));
+          "http://" + LOCAL_ADDR_IPV6 + ":" + (pdServer.port + 2),
+          client.getPdClientWrapper().getLeaderInfo());
     }
   }
 
