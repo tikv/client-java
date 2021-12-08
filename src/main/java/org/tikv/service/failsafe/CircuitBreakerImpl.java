f3f8869cd4a650fddf06965c85c3c9465cfdb815
marsishandsome <marsishandsome@gmail.com>
2021-12-08 16:51:27 +0800

    remove error metrics
    Signed-off-by: marsishandsome <marsishandsome@gmail.com>

--- src/main/java/org/tikv/service/failsafe/CircuitBreakerImpl.java
+++ src/main/java/org/tikv/service/failsafe/CircuitBreakerImpl.java
@@ -16,7 +16,6 @@
 package org.tikv.service.failsafe;
 
 import io.prometheus.client.Counter;
-import io.prometheus.client.Gauge;
 import java.io.IOException;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.atomic.AtomicReference;
@@ -26,12 +25,6 @@ import org.tikv.common.TiConfiguration;
 
 public class CircuitBreakerImpl implements CircuitBreaker {
   private static final Logger logger = LoggerFactory.getLogger(CircuitBreakerImpl.class);
-
-  private static final Gauge CIRCUIT_BREAKER_STATUS =
-      Gauge.build()
-          .name("client_java_circuit_breaker_status")
-          .help("client circuit breaker status.")
-          .register();
 
   private static final Counter CIRCUIT_BREAKER_ATTEMPT_COUNTER =
       Counter.build()
@@ -171,7 +164,6 @@ public class CircuitBreakerImpl implements CircuitBreaker {
       // it sets the start time for the sleep window
       circuitOpened.set(System.currentTimeMillis());
       logger.info("CLOSED => OPEN");
-      CIRCUIT_BREAKER_STATUS.set(Status.OPEN.getValue());
     }
   }
 
@@ -180,7 +172,6 @@ public class CircuitBreakerImpl implements CircuitBreaker {
       // This thread wins the race to close the circuit
       circuitOpened.set(-1L);
       logger.info("HALF_OPEN => CLOSED");
-      CIRCUIT_BREAKER_STATUS.set(Status.CLOSED.getValue());
     }
   }
 
@@ -191,7 +182,6 @@ public class CircuitBreakerImpl implements CircuitBreaker {
       attemptCount.set(0);
       attemptSuccessCount.set(0);
       logger.info("OPEN => HALF_OPEN");
-      CIRCUIT_BREAKER_STATUS.set(Status.HALF_OPEN.getValue());
     }
   }
 
@@ -201,7 +191,6 @@ public class CircuitBreakerImpl implements CircuitBreaker {
       // it resets the start time for the sleep window
       circuitOpened.set(System.currentTimeMillis());
       logger.info("HALF_OPEN => OPEN");
-      CIRCUIT_BREAKER_STATUS.set(Status.OPEN.getValue());
     }
   }
 
