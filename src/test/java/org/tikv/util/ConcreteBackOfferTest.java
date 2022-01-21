ecaa89e52a520f54072ba07930a0bb1a41ee9f90
iosmanthus <myosmanthustree@gmail.com>
2022-01-21 14:59:30 +0800

    fix license error
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- src/test/java/org/tikv/util/ConcreteBackOfferTest.java
+++ src/test/java/org/tikv/util/ConcreteBackOfferTest.java
@@ -1,22 +1,10 @@
 /*
-<<<<<<< HEAD
- * Copyright 2017 PingCAP, Inc.
-=======
  * Copyright 2021 TiKV Project Authors.
->>>>>>> a7e0e74b... [close #489 ] fix backoffer data race (#490)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
-<<<<<<< HEAD
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * See the License for the specific language governing permissions and
- * limitations under the License.
-=======
  * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
@@ -25,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
->>>>>>> a7e0e74b... [close #489 ] fix backoffer data race (#490)
  */
 
 package org.tikv.util;
