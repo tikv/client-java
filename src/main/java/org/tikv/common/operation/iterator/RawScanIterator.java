6b03d82879f4703e9fa677840e49d0d1a8efe4ef
Liangliang Gu <marsishandsome@gmail.com>
2022-03-01 14:42:28 +0800

    Revert "cherry pick #541 to release-3.1 (#545)"
    This reverts commit ad6a00faba1dad0b8d5ae14b47b81066c7cbb079.

--- src/main/java/org/tikv/common/operation/iterator/RawScanIterator.java
+++ src/main/java/org/tikv/common/operation/iterator/RawScanIterator.java
@@ -28,7 +28,6 @@ import org.tikv.common.util.BackOffer;
 import org.tikv.kvproto.Kvrpcpb;
 
 public class RawScanIterator extends ScanIterator {
-
   private final BackOffer scanBackOffer;
 
   public RawScanIterator(
@@ -66,12 +65,11 @@ public class RawScanIterator extends ScanIterator {
     }
   }
 
-  private boolean endOfScan() {
-    if (!processingLastBatch) {
-      return false;
-    }
-    ByteString lastKey = currentCache.get(index).getKey();
-    return !lastKey.isEmpty() && Key.toRawKey(lastKey).compareTo(endKey) >= 0;
+  private boolean notEndOfScan() {
+    return limit > 0
+        && !(processingLastBatch
+            && (index >= currentCache.size()
+                || Key.toRawKey(currentCache.get(index).getKey()).compareTo(endKey) >= 0));
   }
 
   boolean isCacheDrained() {
@@ -90,7 +88,7 @@ public class RawScanIterator extends ScanIterator {
         return false;
       }
     }
-    return !endOfScan();
+    return notEndOfScan();
   }
 
   private Kvrpcpb.KvPair getCurrent() {
