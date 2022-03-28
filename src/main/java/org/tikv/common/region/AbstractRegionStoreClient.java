8ff27f29a6e4e4fe8453d721c60a2efe1ea703d1
iosmanthus <myosmanthustree@gmail.com>
2022-03-28 14:48:08 +0800

    ./dev/javafmt
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- src/main/java/org/tikv/common/region/AbstractRegionStoreClient.java
+++ src/main/java/org/tikv/common/region/AbstractRegionStoreClient.java
@@ -104,8 +104,7 @@ public abstract class AbstractRegionStoreClient
   }
 
   @Override
-  public void close() throws GrpcException {
-  }
+  public void close() throws GrpcException {}
 
   /**
    * onNotLeader deals with NotLeaderError and returns whether re-splitting key range is needed
