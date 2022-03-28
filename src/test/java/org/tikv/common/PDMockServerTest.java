e3e4fe7c122b203ba68cd1bfd87a91437726834b
ti-srebot <66930949+ti-srebot@users.noreply.github.com>
2022-03-28 19:27:38 +0800

    [close #550] rawkv: fix seek leader/proxy store early abort (#551) (#565)

--- src/test/java/org/tikv/common/PDMockServerTest.java
+++ src/test/java/org/tikv/common/PDMockServerTest.java
@@ -18,40 +18,59 @@
 package org.tikv.common;
 
 import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 
 public abstract class PDMockServerTest {
   protected static final String LOCAL_ADDR = "127.0.0.1";
   static final long CLUSTER_ID = 1024;
-  protected static TiSession session;
-  protected PDMockServer pdServer;
+  protected TiSession session;
+  protected PDMockServer leader;
+  protected List<PDMockServer> pdServers = new ArrayList<>();
 
   @Before
-  public void setUp() throws IOException {
-    setUp(LOCAL_ADDR);
+  public void setup() throws IOException {
+    setup(LOCAL_ADDR);
   }
 
-  void setUp(String addr) throws IOException {
-    pdServer = new PDMockServer();
-    pdServer.start(CLUSTER_ID);
-    pdServer.addGetMemberResp(
-        GrpcUtils.makeGetMembersResponse(
-            pdServer.getClusterId(),
-            GrpcUtils.makeMember(1, "http://" + addr + ":" + pdServer.port),
-            GrpcUtils.makeMember(2, "http://" + addr + ":" + (pdServer.port + 1)),
-            GrpcUtils.makeMember(3, "http://" + addr + ":" + (pdServer.port + 2))));
-    TiConfiguration conf = TiConfiguration.createDefault(addr + ":" + pdServer.port);
-    conf.setEnableGrpcForward(false);
+  void setup(String addr) throws IOException {
+    int basePort;
+    try (ServerSocket s = new ServerSocket(0)) {
+      basePort = s.getLocalPort();
+    }
+
+    for (int i = 0; i < 3; i++) {
+      PDMockServer server = new PDMockServer();
+      server.start(CLUSTER_ID, basePort + i);
+      server.addGetMembersListener(
+          (request) ->
+              GrpcUtils.makeGetMembersResponse(
+                  server.getClusterId(),
+                  GrpcUtils.makeMember(1, "http://" + addr + ":" + basePort),
+                  GrpcUtils.makeMember(2, "http://" + addr + ":" + (basePort + 1)),
+                  GrpcUtils.makeMember(3, "http://" + addr + ":" + (basePort + 2))));
+      pdServers.add(server);
+      if (i == 0) {
+        leader = server;
+      }
+    }
+
+    TiConfiguration conf = TiConfiguration.createDefault(addr + ":" + leader.port);
     conf.setKvMode("RAW");
-    conf.setTest(true);
+    conf.setWarmUpEnable(false);
     conf.setTimeout(2000);
+    conf.setEnableGrpcForward(true);
     session = TiSession.create(conf);
   }
 
   @After
   public void tearDown() throws Exception {
     session.close();
-    pdServer.stop();
+    for (PDMockServer server : pdServers) {
+      server.stop();
+    }
   }
 }
