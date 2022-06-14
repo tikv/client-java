fdf0e13c729e479dc2d09e1452642fd9f5949432
Daemonxiao <735462752@qq.com>
2022-06-14 17:18:46 +0800

    update ChannelFactoryTest.java Signed-off-by: Daemonxiao <735462752@qq.com> Co-authored-by: iosmanthus <myosmanthustree@gmail.com>

--- src/test/java/org/tikv/common/ChannelFactoryTest.java
+++ src/test/java/org/tikv/common/ChannelFactoryTest.java
@@ -25,8 +25,6 @@ import io.grpc.ManagedChannel;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
