e04c7ac141e04610b8f592a72d816d27b94731d3
iosmanthus <myosmanthustree@gmail.com>
2022-06-18 01:11:09 +0800

    accept pingyu's suggestion
    Signed-off-by: iosmanthus <myosmanthustree@gmail.com>

--- src/main/java/org/tikv/common/TiSession.java
+++ src/main/java/org/tikv/common/TiSession.java
@@ -176,7 +176,8 @@ public class TiSession implements AutoCloseable {
 
     this.client = PDClient.createRaw(conf, keyCodec, channelFactory);
     if (conf.getApiVersion().isV2() && !StoreVersion.minTiKVVersion(Version.API_V2, client)) {
-      throw new IllegalStateException("Store version should larger than " + Version.API_V2);
+      throw new IllegalStateException(
+          "With API v2, store versions should not older than " + Version.API_V2);
     }
 
     this.enableGrpcForward = conf.getEnableGrpcForward();
