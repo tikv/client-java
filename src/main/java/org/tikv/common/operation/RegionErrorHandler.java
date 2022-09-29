b2db42ec867525873cb2cd93737a1aa9458ac128
qidi1 <1083369179@qq.com>
2022-09-29 09:39:20 +0800

    add comment
    Signed-off-by: qidi1 <1083369179@qq.com>

--- src/main/java/org/tikv/common/operation/RegionErrorHandler.java
+++ src/main/java/org/tikv/common/operation/RegionErrorHandler.java
@@ -277,7 +277,8 @@ public class RegionErrorHandler<RespT> implements ErrorHandler<RespT> {
   private void notifyRegionRequestError(
       TiRegion ctxRegion, long storeId, CacheInvalidateEvent.CacheType type) {
     CacheInvalidateEvent event;
-    // When store(region) id is 0, it implies that the error was not caused by store(region).
+    // When store(region) id is invalid,
+    // it implies that the error was not caused by store(region) error.
     switch (type) {
       case REGION:
       case LEADER:
