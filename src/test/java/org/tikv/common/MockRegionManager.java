8eee5d5fb1a6bc1ee72851c15bdc689f3e9b05e8
shiyuhang <1136742008@qq.com>
2022-12-23 17:30:52 +0800

    Add test for RangeSplitter
    Signed-off-by: shiyuhang <1136742008@qq.com>

---
+++ src/test/java/org/tikv/common/MockRegionManager.java
@@ -0,0 +1,72 @@
+package org.tikv.common;
+
+import static org.tikv.common.GrpcUtils.encodeKey;
+
+import com.google.common.collect.ImmutableList;
+import com.google.protobuf.ByteString;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import org.tikv.common.apiversion.RequestKeyV1TxnCodec;
+import org.tikv.common.key.Key;
+import org.tikv.common.region.RegionManager;
+import org.tikv.common.region.TiRegion;
+import org.tikv.common.region.TiStore;
+import org.tikv.common.region.TiStoreType;
+import org.tikv.common.util.KeyRangeUtils;
+import org.tikv.common.util.Pair;
+import org.tikv.kvproto.Coprocessor.KeyRange;
+import org.tikv.kvproto.Kvrpcpb.CommandPri;
+import org.tikv.kvproto.Kvrpcpb.IsolationLevel;
+import org.tikv.kvproto.Metapb;
+import org.tikv.kvproto.Metapb.Peer;
+import org.tikv.kvproto.Metapb.Region;
+
+public class MockRegionManager extends RegionManager {
+
+  private final Map<KeyRange, TiRegion> mockRegionMap;
+
+  private static TiRegion region(long id, KeyRange range) {
+    RequestKeyV1TxnCodec v1 = new RequestKeyV1TxnCodec();
+
+    TiConfiguration configuration = new TiConfiguration();
+    configuration.setIsolationLevel(IsolationLevel.RC);
+    configuration.setCommandPriority(CommandPri.Low);
+    Region r =
+        Metapb.Region.newBuilder()
+            .setRegionEpoch(Metapb.RegionEpoch.newBuilder().setConfVer(1).setVersion(2))
+            .setId(id)
+            .setStartKey(encodeKey(range.getStart().toByteArray()))
+            .setEndKey(encodeKey(range.getEnd().toByteArray()))
+            .addPeers(Peer.getDefaultInstance())
+            .build();
+
+    List<Metapb.Store> s = ImmutableList.of(Metapb.Store.newBuilder().setId(id).build());
+
+    return new TiRegion(
+        configuration,
+        v1.decodeRegion(r),
+        null,
+        r.getPeersList(),
+        s.stream().map(TiStore::new).collect(Collectors.toList()));
+  }
+
+  public MockRegionManager(List<KeyRange> ranges) {
+    super(null, null);
+    mockRegionMap =
+        ranges.stream().collect(Collectors.toMap(kr -> kr, kr -> region(ranges.indexOf(kr), kr)));
+  }
+
+  @Override
+  public Pair<TiRegion, TiStore> getRegionStorePairByKey(ByteString key, TiStoreType storeType) {
+    for (Map.Entry<KeyRange, TiRegion> entry : mockRegionMap.entrySet()) {
+      KeyRange range = entry.getKey();
+      if (KeyRangeUtils.makeRange(range.getStart(), range.getEnd()).contains(Key.toRawKey(key))) {
+        TiRegion region = entry.getValue();
+        return Pair.create(
+            region, new TiStore(Metapb.Store.newBuilder().setId(region.getId()).build()));
+      }
+    }
+    return null;
+  }
+}
