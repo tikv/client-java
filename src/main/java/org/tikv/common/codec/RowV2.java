cd999f977b0e48e6311d0febb054be1c8b03cc96
ab111404212 <18217007736@163.com>
2023-02-08 17:47:01 +0800

    fix byte array overflow Signed-off-by: wuyj@shuyilink.com

--- src/main/java/org/tikv/common/codec/RowV2.java
+++ src/main/java/org/tikv/common/codec/RowV2.java
@@ -147,7 +147,7 @@ public class RowV2 {
       if (this.large) {
         v = this.colIDs32[h];
       } else {
-        v = this.colIDs[h];
+        v = this.colIDs[h] & 0xFF;
       }
       if (v < colID) {
         i = h + 1;
